7dc36a9ae16fa7f5b388f64e0e907e32e7910adc
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2021-12-03 18:01:40 -0800

    Added video play button

--- src/lib/components/IntersectionOberver.svelte
+++ src/lib/components/IntersectionOberver.svelte
@@ -1,9 +1,8 @@
 <script>
 	import { onMount } from 'svelte';
 	let main;
-	let isPaused = false;
 	let offX = '0px';
-	let offY = '-200px';
+	let offY = '0px';
 
 	onMount(() => {
 		const lazyMedia = main.querySelectorAll('.lazy-media');
@@ -11,30 +10,22 @@
 		let lazyMediaObserver = new IntersectionObserver(
 			(entries) => {
 				entries.forEach((entry, index) => {
-					// console.log(index);
 					if (entry.target.nodeName === 'IMG') {
-						// console.log('Image: ' + entry.isIntersecting);
 						if (entry.isIntersecting) {
 							let image = entry.target;
 							image.src = image.dataset.src;
 							setTimeout(() => {
-								// if (image.complete) {
-								// console.log('image complete', image);
 								image.classList.add('transition-fade');
-								// }
 							}, 500);
 							lazyMediaObserver.unobserve(image);
 						}
 					}
 					if (entry.target.nodeName === 'VIDEO') {
-						// console.log('Video: ' + entry.isIntersecting);
 						let video = entry.target;
 						if (entry.isIntersecting && video.paused === true) {
 							video.play();
-							console.log(video.paused);
 						} else if (!entry.isIntersecting && video.paused === false) {
 							video.pause();
-							console.log(video.paused);
 						}
 					}
 				});
--- src/lib/components/Video.svelte
+++ src/lib/components/Video.svelte
@@ -1,11 +1,13 @@
 <script>
 	import { onMount } from 'svelte';
+	import { fade } from 'svelte/transition';
 	export let src;
 	export let autoplay = 'true';
 	export let loop = 'true';
 	export let muted = 'true';
 	export let width = 'full';
 	let video;
+	let paused = true;
 
 	onMount(() => {
 		video.src = video.dataset.src;
@@ -22,13 +24,25 @@
 		src=""
 		data-src={src}
 		on:click={video.play()}
+		bind:paused
 	>
 		<track kind="captions" />
 		Sorry, your browser doesn't support embedded videos.
 	</video>
-	<!-- {#if video.paused} -->
-	<!-- <div class="absolute top-0 left-0 w-full h-full bg-red-500 bg-opacity-50" /> -->
-	<!-- {/if} -->
+	{#if paused}
+		<div
+			class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex justify-center items-center w-40 h-40 rounded-full bg-blue-500 bg-opacity-80 text-white"
+			on:click={video.play()}
+			transition:fade={{ duration: 250 }}
+		>
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="64" height="64"
+				><path fill="none" d="M0 0h24v24H0z" /><path
+					d="M19.376 12.416L8.777 19.482A.5.5 0 0 1 8 19.066V4.934a.5.5 0 0 1 .777-.416l10.599 7.066a.5.5 0 0 1 0 .832z"
+					fill="currentColor"
+				/></svg
+			>
+		</div>
+	{/if}
 </div>
 
 <style>
